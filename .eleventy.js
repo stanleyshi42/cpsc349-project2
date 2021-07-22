26bf4bb485d06f18122f34da726a54e690eb0a4c
quentinmay <quentinemay@gmail.com>
2021-07-21 20:59:47 -0700

    Completely rerouted all directories for photos, base, flex, grid, and tail.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("styles/main.css");
+    eleventyConfig.addPassthroughCopy("styles/grid.css");
+    eleventyConfig.addPassthroughCopy("styles/flex.css");
 };
--- unsplash/photos.py
+++ unsplash/photos.py
@@ -12,7 +12,58 @@ with open(sys.argv[1]) as tsvfile:
         description = row.pop('photo_description')
         row['tags'] = 'photos'
         row['layout'] = 'pets'
-        with open(f'../photos/{n:03}.html', 'w') as f:
+        with open(f'../base/photos/{n:03}.html', 'w') as f:
+            yaml.dump(row, f, explicit_start=True)
+            print('---', file=f)
+            html = f"""    <figure>
+        <img src="{{{{ photo_image_url }}}}" height="600" />
+        <figcaption>{description}</figcaption>
+    </figure>"""
+            print(html, file=f)
+        n += 1
+
+with open(sys.argv[1]) as tsvfile:
+    photoreader = csv.DictReader(tsvfile, delimiter='\t')
+    n = 0
+    for row in photoreader:
+        description = row.pop('photo_description')
+        row['tags'] = 'photos'
+        row['layout'] = 'petsGrid'
+        with open(f'../grid/photos/{n:03}.html', 'w') as f:
+            yaml.dump(row, f, explicit_start=True)
+            print('---', file=f)
+            html = f"""    <figure>
+        <img src="{{{{ photo_image_url }}}}" height="600" />
+        <figcaption>{description}</figcaption>
+    </figure>"""
+            print(html, file=f)
+        n += 1
+
+with open(sys.argv[1]) as tsvfile:
+    photoreader = csv.DictReader(tsvfile, delimiter='\t')
+    n = 0
+    for row in photoreader:
+        description = row.pop('photo_description')
+        row['tags'] = 'photos'
+        row['layout'] = 'petsFlex'
+        with open(f'../flex/photos/{n:03}.html', 'w') as f:
+            yaml.dump(row, f, explicit_start=True)
+            print('---', file=f)
+            html = f"""    <figure>
+        <img src="{{{{ photo_image_url }}}}" height="600" />
+        <figcaption>{description}</figcaption>
+    </figure>"""
+            print(html, file=f)
+        n += 1
+
+with open(sys.argv[1]) as tsvfile:
+    photoreader = csv.DictReader(tsvfile, delimiter='\t')
+    n = 0
+    for row in photoreader:
+        description = row.pop('photo_description')
+        row['tags'] = 'photos'
+        row['layout'] = 'petsTail'
+        with open(f'../tail/photos/{n:03}.html', 'w') as f:
             yaml.dump(row, f, explicit_start=True)
             print('---', file=f)
             html = f"""    <figure>
