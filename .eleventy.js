7a15eca0548701693df220f1c536bb5e505b0010
quentinmay <quentinemay@gmail.com>
2021-07-24 20:02:32 -0700

    Fixed pathPrefix not working

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("styles");
+    return {
+        pathPrefix: "/cpsc349-project2"
+    }
 };
